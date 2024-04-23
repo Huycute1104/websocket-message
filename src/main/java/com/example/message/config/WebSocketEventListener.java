0f45239b505570d93470c780c094ee9726ea4ab7
Huy Pham <huypt110402@gmail.com>
2024-04-23 21:12:27 +0700

    add controller

--- src/main/java/com/example/message/config/WebSocketEventListener.java
+++ src/main/java/com/example/message/config/WebSocketEventListener.java
@@ -1,12 +1,34 @@
 package com.example.message.config;
 
+import com.example.message.chat.ChatMessage;
+import com.example.message.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
+    private final SimpMessageSendingOperations messageTemplate;
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            //This message template help convert and send the payload to the socket destination
+            //Help alert other users that there is someone left
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
 
 }
---
+++ src/main/java/com/example/message/controller/ChatController.java
@@ -0,0 +1,7 @@
+package com.example.message.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+}
