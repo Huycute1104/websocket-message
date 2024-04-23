e10caef3e66f4608df3f1cfd21e0799f71509fa0
Huy Pham <huypt110402@gmail.com>
2024-04-23 21:07:08 +0700

    add model

---
+++ src/main/java/com/example/message/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.example.message.chat;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
